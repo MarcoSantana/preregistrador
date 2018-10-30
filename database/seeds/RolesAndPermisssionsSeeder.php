a5cd4972e539f94162c38fbfb45ce16f47a63cff
Marco Antonio Santana <marco.santana@gmail.com>
2018-10-29 20:17:18 -0600

    Created Roles and permissions seeder. Created Users seeder with attached roles

---
+++ database/seeds/RolesAndPermisssionsSeeder.php
@@ -0,0 +1,33 @@
+<?php
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        // create permissions
+        Permission::create(['name' => 'edit documents']);
+        Permission::create(['name' => 'delete documents']);
+        Permission::create(['name' => 'publish documents']);
+        Permission::create(['name' => 'unpublish documents']);
+        // Permission::create(['name' => 'create jobs']);
+        // Permission::create(['name' => 'see jobs']);
+
+        // create roles and assign created permissions
+
+        $role = Role::create(['name' => 'supervisor']);
+        $role->givePermissionTo('edit documents');
+        $role->givePermissionTo('unpublish documents');
+
+        $role = Role::create(['name' => 'worker']);
+        $role->givePermissionTo(['publish document', 'unpublish document']);
+
+        $role = Role::create(['name' => 'super-admin']);
+        $role->givePermissionTo(Permission::all());
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(App\User::class, 15)->create()->each(function ($u)
+        {
+          $u->assignRole('worker');
+        });
+
+        factory(App\User::class, 3)->create()->each(function ($u)
+        {
+          $u->assignRole('supervisor');
+        });
+
+        factory(App\User::class, 1)->create()->each(function ($u)
+        {
+          $u->assignRole('super-admin');
+        });
+
+    }
+}
